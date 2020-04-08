507b1724c0d33eefea03e8d0574676aa99e784c1
517435708 <a500573435708@gmail.com>
2020-04-09 00:53:12 +0200

    architecture added

--- securevoip/src/main/java/com/bluevortexflare/securevoip/users/session/BasicUserSessionService.java
+++ securevoip/src/main/java/com/bluevortexflare/securevoip/users/session/BasicUserSessionService.java
@@ -1,12 +1,17 @@
 package com.bluevortexflare.securevoip.users.session;
 
+import com.bluevortexflare.securevoip.users.connection.BasicUserConnectionService;
 import com.bluevortexflare.securevoip.users.session.dto.SessionResponse;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BasicUserSessionService implements UserSessionService {
     @Override
     public SessionResponse refreshToken(String userId, String password) {
-        return null;
+
+
+
+
     }
 }
