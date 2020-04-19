afa205fa3652d3c496761533e97908cb377ca8ff
517435708 <a500573435708@gmail.com>
2020-04-19 12:52:39 +0200

    init project client

---
+++ client/app/src/main/java/pl/pp/tiplab/securevoipclient/MainActivity.java
@@ -0,0 +1,13 @@
+package pl.pp.tiplab.securevoipclient;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
