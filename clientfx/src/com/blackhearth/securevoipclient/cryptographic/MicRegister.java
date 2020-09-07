7805811ee23439bb63b12f4fce9499e8264caf24
Marcin Kubiak <rejwen10@interia.pl>
2020-09-07 19:48:52 +0200

    added sending and receiving message

--- clientfx/src/com/blackhearth/securevoipclient/cryptographic/MicRegister.java
+++ clientfx/src/com/blackhearth/securevoipclient/cryptographic/MicRegister.java
@@ -2,12 +2,21 @@ package com.blackhearth.securevoipclient.cryptographic;
 
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
-
+import javax.crypto.Cipher;
 import javax.crypto.BadPaddingException;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
+import javax.sound.sampled.LineUnavailableException;
 
 public interface MicRegister {
-    byte[] sendVoiceMessage();
-    void receiveMessage(byte[] encryptedVoice);
+    byte[] sendVoiceMessage() throws
+                              InvalidKeyException,
+                              BadPaddingException,
+                              IllegalBlockSizeException,
+                              LineUnavailableException;
+    void receiveMessage(byte[] encryptedVoice) throws
+                                               InvalidKeyException,
+                                               BadPaddingException,
+                                               IllegalBlockSizeException,
+                                               LineUnavailableException;
 }
